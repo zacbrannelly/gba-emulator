05e3f828d7667bdfe6ef772d7b04b2df6185045f
Zac Brannelly <zac.brannelly@gmail.com>
2024-08-23 01:11:36 +1000

    Allow viewing multiple data in the ram debugger window

--- ram_debugger.cpp
+++ ram_debugger.cpp
@@ -3,21 +3,43 @@
 #include "3rdparty/zengine/ZEngine-Core/Rendering/Texture2D.h"
 #include "3rdparty/zengine/ZEngine-Core/ImmediateUI/imgui-includes.h"
 
+enum class DataType {
+  BYTE = 0,
+  HALFWORD = 1,
+  WORD = 2
+};
+
 static uint32_t ram_debugger_address = 0;
+static int ram_debugger_dtype = 0;
+static uint32_t ram_debugger_length = 16;
 static uint32_t stack_debugger_length = 16;
 
 void ram_debugger_window(CPU& cpu) {
   if (ImGui::Begin("RAM Debugger")) {
     ImGui::InputScalar("RAM Address", ImGuiDataType_U32, &ram_debugger_address, 0, 0, "%08X", ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::Combo("Data Type", &ram_debugger_dtype, "Byte\0Halfword\0Word\0\0");
+    ImGui::InputScalar("Data Length", ImGuiDataType_U32, &ram_debugger_length, 0, 0, "%d", ImGuiInputTextFlags_CharsDecimal);
 
-    uint8_t byte = ram_read_byte_direct(cpu.ram, ram_debugger_address);
-    ImGui::Text("Byte: 0x%02X", byte);
-
-    uint16_t halfword = ram_read_half_word_direct(cpu.ram, ram_debugger_address);
-    ImGui::Text("Halfword: 0x%04X", halfword);
-
-    uint32_t word = ram_read_word_direct(cpu.ram, ram_debugger_address);
-    ImGui::Text("Word: 0x%08X", word);
+    ImGui::BeginListBox("RAM Data", ImVec2(0, 200 + ram_debugger_length * 20));
+    for (int i = 0; i < ram_debugger_length; i++) {
+      uint32_t address = ram_debugger_address + i * (1 << ram_debugger_dtype);
+      uint32_t data = 0;
+      switch ((DataType)ram_debugger_dtype) {
+        case DataType::BYTE:
+          data = ram_read_byte_direct(cpu.ram, address);
+          ImGui::Text("0x%08X: 0x%02X", address, data);
+          break;
+        case DataType::HALFWORD:
+          data = ram_read_half_word_direct(cpu.ram, address);
+          ImGui::Text("0x%08X: 0x%04X", address, data);
+          break;
+        case DataType::WORD:
+          data = ram_read_word_direct(cpu.ram, address);
+          ImGui::Text("0x%08X: 0x%08X", address, data);
+          break;
+      }
+    }
+    ImGui::EndListBox();
   }
   ImGui::End();
 
